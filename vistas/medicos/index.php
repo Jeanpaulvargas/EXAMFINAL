086cfd6629f2c89a4f1354cdbb78aff68131a66c
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-06-10 00:50:18 -0600

    mejora de aspecto index medico

--- vistas/medicos/index.php
+++ vistas/medicos/index.php
@@ -1,46 +1,36 @@
-<?php
+<?php include_once '../../vistas/templates/header.php'; ?>
 
-include_once '../../vistas/templates/header.php';
-
-?>
-
-<h1 class="text-center">FORMULARIO PARA MEDICOS</h1>
+<h1 class="text-center">FORMULARIO PARA MÉDICOS</h1>
 <div class="row justify-content-center">
     <form action="../../controladores/medicos/guardar.php" method="POST" class="border bg-light shadow rounded p-4 col-lg-6">
         <div class="row mb-3">
             <div class="col">
-                <label for="medi_nombres">INGRESE SUS NOMBRES</label>
+                <label for="medi_nombres">Nombres</label>
                 <input type="text" name="medi_nombres" id="medi_nombres" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="medi_apellidos">INGRESE SUS APELLIDOS</label>
+                <label for="medi_apellidos">Apellidos</label>
                 <input type="text" name="medi_apellidos" id="medi_apellidos" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="medi_especialidad">INGRESE SU ESPECIALIDAD</label>
+                <label for="medi_especialidad">Especialidad</label>
                 <input type="text" name="medi_especialidad" id="medi_especialidad" class="form-control" required>
             </div>
         </div>
 
-
-
-
-        <div class="row mb-3">
-            <div class="col">
-                <button type="submit" class="btn btn-success w-100">GUARDAR</button>
-            </div>
-        </div>
         <div class="row">
             <div class="col">
-                <a href="../../controladores/medicos/buscar.php" class="btn btn-primary w-100">BUSCAR</a>
+                <button type="submit" class="btn btn-success w-100">Guardar</button>
+            </div>
+            <div class="col">
+                <a href="../../controladores/medicos/buscar.php" class="btn btn-primary w-100">Buscar</a>
             </div>
         </div>
     </form>
 </div>
-
 
 <?php include_once '../../vistas/templates/footer.php'; ?>
