0a1a699bcb2a443afc7b8370068aa85aa150a31d
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-06-10 00:45:46 -0600

    Se agrego el icono del hospital

--- vistas/templates/footer.php
+++ vistas/templates/footer.php
@@ -1,4 +1,5 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha384-Gv6CqjNIneJxEyF6KAkJjymMF5d8tWdvqHAuTtLXNbPfVG79qAFOeNVg0Y4qYVzZ" crossorigin="anonymous">
 </body>
 </html>
--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -1,45 +1,58 @@
-<nav class="navbar fixed-top navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="../../vistas/pacientes/index.php">
-      <img src="../../images/icono.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2">
-      HOSPITAL
+      <img src="../../IMAGES/logoM-removebg-preview.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top me-2">
+      <span style="font-size: 1.5rem;">HOSPITAL LA ESPERANZA</span>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+    <div class="collapse navbar-collapse justify-content-start" id="navbarScroll">
+      <ul class="navbar-nav">
+       
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">
-            <i class="bi bi-house-door-fill me-2"></i>INICIO
+          <a class="nav-link active" aria-current="page" href="#">
+            <i class="bi bi-house-door-fill me-1"></i><span style="font-size: 1.2rem;">INICIO</span>
           </a>
         </li>
+      
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-person-fill me-2"></i>PACIENTES
+            <i class="bi bi-person-fill me-1"></i><span style="font-size: 1.2rem;">PACIENTES</span>
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../../vistas/pacientes/index.php"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
-            <li><a class="dropdown-item" href="../pacientes/buscar.php"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
+            <li><a class="dropdown-item" href="../../vistas/pacientes/index.php"><i class="bi bi-person-plus-fill me-1"></i><span style="font-size: 1.2rem;">CREAR</span></a></li>
+            <li><a class="dropdown-item" href="../pacientes/buscar.php"><i class="bi bi-search me-1"></i><span style="font-size: 1.2rem;">BUSCAR</span></a></li>
           </ul>
         </li>
-        </li>
+   
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-person-fill me-2"></i>MEDICOS
+            <i class="bi bi-person-fill me-1"></i><span style="font-size: 1.2rem;">MÉDICOS</span>
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../../vistas/medicos/index.php"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
-            <li><a class="dropdown-item" href="../medicos/buscar.php"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
+            <li><a class="dropdown-item" href="../../vistas/medicos/index.php"><i class="bi bi-people-fill me-1"></i><span style="font-size: 1.2rem;">CREAR</span></a></li>
+            <li><a class="dropdown-item" href="../medicos/buscar.php"><i class="bi bi-search me-1"></i><span style="font-size: 1.2rem;">BUSCAR</span></a></li>
           </ul>
         </li>
+      
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-person-fill me-2"></i>CLINICAS
+            <i class="bi bi-building-fill me-1"></i><span style="font-size: 1.2rem;">CLÍNICAS</span>
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../../vistas/clinicas/index.php"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
-            <li><a class="dropdown-item" href="../clinicas/buscar.php"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
+            <li><a class="dropdown-item" href="../../vistas/clinicas/index.php"><i class="bi bi-building-plus-fill me-1"></i><span style="font-size: 1.2rem;">CREAR</span></a></li>
+            <li><a class="dropdown-item" href="../clinicas/buscar.php"><i class="bi bi-search me-1"></i><span style="font-size: 1.2rem;">BUSCAR</span></a></li>
+          </ul>
+        </li>
+      
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-calendar-fill me-1"></i><span style="font-size: 1.2rem;">CITAS</span>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="../../vistas/citas/index.php"><i class="bi bi-calendar-plus-fill me-1"></i><span style="font-size: 1.2rem;">CREAR</span></a></li>
+            <li><a class="dropdown-item" href="../citas/buscar.php"><i class="bi bi-search me-1"></i><span style="font-size: 1.2rem;">BUSCAR</span></a></li>
           </ul>
         </li>
       </ul>
